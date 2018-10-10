94f809b8822ab103ce3ad694dbb950e707853013
philipperemy <premy@cogent.co.jp>
2018-10-10 13:46:43 +0900

    finalization

---
+++ query.py
@@ -0,0 +1,45 @@
+class NameDataset:
+    FIRST_NAME_SEARCH = 'FIRST_NAME_SEARCH'
+    LAST_NAME_SEARCH = 'LAST_NAME_SEARCH'
+
+    def __init__(self):
+        with open('first_names.all.txt', 'r', errors='ignore', encoding='utf8') as r:
+            self.first_names = r.read().strip().split('\n')
+        with open('last_names.all.txt', 'r', errors='ignore', encoding='utf8') as r:
+            self.last_names = r.read().strip().split('\n')
+
+    def search_name(self, name, name_type):
+        names = self.first_names if name_type == NameDataset.FIRST_NAME_SEARCH else self.last_names
+        return name.strip().lower() in names
+
+    def search_first_name(self, first_name):
+        return self.search_name(first_name, name_type=NameDataset.FIRST_NAME_SEARCH)
+
+    def search_last_name(self, last_name):
+        return self.search_name(last_name, name_type=NameDataset.LAST_NAME_SEARCH)
+
+
+if __name__ == '__main__':
+    import sys
+
+    if sys.version_info < (3, 0):
+        print('Please use Python3+')
+        exit(1)
+
+    if len(sys.argv) < 2:
+        print('Give names separated by a comma as input.')
+        sys.exit(1)
+    m = NameDataset()
+    names_list = sys.argv[1].split(',')
+    print('----- First names ----')
+    print('Name'.ljust(30), 'Present?')
+    for name in names_list:
+        # ljust just for aesthetic reasons ;)
+        print(str(name).ljust(30), m.search_first_name(name))
+
+    print('----- Last names ----')
+    print('Name'.ljust(30), 'Present?')
+    for name in names_list:
+        # ljust just for aesthetic reasons ;)
+        print(str(name).ljust(30), m.search_last_name(name))
+    sys.exit(0)
