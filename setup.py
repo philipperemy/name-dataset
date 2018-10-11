04c9311f1bd22f23aadb6f5b1f24eaa20a3d20ed
philipperemy <premy@cogent.co.jp>
2018-10-11 09:24:06 +0900

    almost there

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+
+package_name = find_packages()[0]
+
+setup(
+    name='names-dataset',
+    version=VERSION,
+    description='Probably the biggest dataset of Names, worldwide',
+    author='Philippe Remy',
+    license='MIT',
+    packages=[package_name],
+    include_package_data=True,
+    data_files=[(package_name, [os.path.join(package_name, 'first_names.all.txt'),
+                                os.path.join(package_name, 'last_names.all.txt')])]
+)
