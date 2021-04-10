07c0418c49383eca311d4a80b23eadc11c678f4b
Philippe Remy <premy.enseirb@gmail.com>
2021-04-10 13:21:03 +0900

    v2

--- names_dataset/__init__.py
+++ names_dataset/__init__.py
@@ -1,2 +1,2 @@
-from names_dataset.nd_v1 import NameDatasetV1
-from names_dataset.nd_v2 import NameDataset
+from names_dataset.nd_v1 import NameDatasetV1  # noqa
+from names_dataset.nd_v2 import NameDataset  # noqa
